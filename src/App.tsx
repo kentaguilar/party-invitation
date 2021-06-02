fc4c45256f2805705a0bae5178296a54795af85f
Kent Aguilar <kentaguilar@gmail.com>
2021-06-02 10:36:57 +0800

    added initial code

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState{
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
+
+function App() {
+  // const [student, setStudent] = useState<{age: number, name: string}[]>([])
+  const [student, setStudent] = useState<IState["people"]>([])
+  const [people, setPeople] = useState([
+    {
+      name: "Lebron James",
+      url: "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png",
+      age: 36,
+      note: "Allergic to staying on the same team"
+    },
+    {
+      name: "Stephen Curry",
+      url: "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3975.png",
+      age: 38,
+      note: "Test"
+    },
+    {
+      name: "Luka Doncic",
+      url: "https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3945274.png",
+      age: 30,
+      note: "Test sample here"
+    }
+  ]);
+
+  // student.map(person => {
+  //   person.name
+  // });
+
+  const [number, setNumber] = useState<any>(5);
+
+  const changeNumber = () => {
+    setNumber("10");
+  }
+
+  return (
+    <div className="App">
+      <h1>People Invited to my Party</h1>
+      <List people={people}></List>
+      <AddToList></AddToList>
+    </div>
+  );
+}
+
+export default App;
